0eaa4291e049687e3c8e102cd67d034879bb9909
Timothy Gu <timothygu99@gmail.com>
2016-08-11 18:25:54 -0700

    Make this module a parser

--- index.js
+++ index.js
@@ -3,12 +3,26 @@
 const {compile} = require('jade')
 const parse = require('posthtml-parser')
 
-module.exports = function (opts) {
-  opts = opts || {}
-  opts.pretty = opts.pretty || true
-  opts.locals = opts.locals || {}
+const defaultOptions = {
+  pretty: true,
+  locals: {}
+};
 
-  return function PostHTMLJade (tree) {
-    return tree.length === 1 ? parse(compile(tree, opts)(opts.locals)) : tree
-  }
+function postHTMLJadeParser(html, options) {
+  return parse(compile(tree, options)(options.locals));
 }
+
+function parserWrapper(html, options) {
+    if (isObject(html)) {
+        options = objectAssign({}, defaultOptions);
+        return function (html) {
+            return postHTMLJadeParser(html, options);
+        };
+    }
+
+    options = objectAssign({}, defaultOptions);
+    return postHTMLJadeParser(html, options);
+}
+
+module.exports = parserWrapper;
+module.exports.defaultOptions = defaultOptions;
